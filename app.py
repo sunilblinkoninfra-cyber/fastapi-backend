99a11366fbc68b32968cdbc2032442a623b1f19a
sunilblinkoninfra-cyber <sunil.blinkoninfra@gmail.com>
2026-01-13 03:21:39 +0530

    Fix indentation and reformat app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+print(">>> RUNNING app.py <<<")
 from fastapi import FastAPI
 from fastapi.openapi.docs import get_swagger_ui_html
 from fastapi.openapi.utils import get_openapi
@@ -31,3 +32,4 @@ def docs():
         openapi_url="/openapi.json",
         title="Swagger Test",
     )
+
